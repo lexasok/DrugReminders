940bcacd51b166fb9bc8cf6b280bd9085d4f2af0
lexa <sokolov@ozero.net>
2018-04-20 18:48:26 +0300

    Person class initialized

--- app/src/main/java/net/ozero/drugreminders/DataStructures/Person.java
+++ app/src/main/java/net/ozero/drugreminders/DataStructures/Person.java
@@ -1,4 +1,16 @@
 package net.ozero.drugreminders.DataStructures;
 
 public class Person {
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
 }
