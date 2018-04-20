4deb9fde5b41e80049f154da915e4d2019ee73fc
lexa <sokolov@ozero.net>
2018-04-20 18:44:29 +0300

    Useless code deleted

--- app/src/main/java/net/ozero/drugreminders/MainActivity.java
+++ app/src/main/java/net/ozero/drugreminders/MainActivity.java
@@ -1,8 +1,10 @@
 package net.ozero.drugreminders;
 
+import android.app.AlarmManager;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.AlarmManagerCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
