b4874b20a94cced37eb0647342cd8d372cd205f4
lexa <sokolov@ozero.net>
2018-04-25 02:38:13 +0300

    Fields added

--- app/src/main/java/net/ozero/drugreminders/DataStructures/Person.java
+++ app/src/main/java/net/ozero/drugreminders/DataStructures/Person.java
@@ -20,4 +20,5 @@ public class Person {
     public void setDrugs(Drug[] drugs) {
         this.drugs = drugs;
     }
+
 }
--- app/src/main/java/net/ozero/drugreminders/adapters/MainActivityPersonsListAdapter.java
+++ app/src/main/java/net/ozero/drugreminders/adapters/MainActivityPersonsListAdapter.java
@@ -4,6 +4,7 @@ import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 public class MainActivityPersonsListAdapter extends RecyclerView.Adapter<MainActivityPersonsListAdapter.PersonsViewHolder>{
     @NonNull
@@ -23,6 +24,9 @@ public class MainActivityPersonsListAdapter extends RecyclerView.Adapter<MainAct
     }
 
     public class PersonsViewHolder extends RecyclerView.ViewHolder{
+        TextView name;
+        TextView drugsCount;
+
         public PersonsViewHolder(View itemView) {
             super(itemView);
 
