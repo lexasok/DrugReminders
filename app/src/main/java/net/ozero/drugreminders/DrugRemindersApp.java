bd4edec19fc093c246ce73aefd22f7184e64e11e
lexa <sokolov@ozero.net>
2018-04-25 02:32:58 +0300

    Added some empty data

--- app/src/main/java/net/ozero/drugreminders/DrugRemindersApp.java
+++ app/src/main/java/net/ozero/drugreminders/DrugRemindersApp.java
@@ -1,7 +1,19 @@
 package net.ozero.drugreminders;
 
 import android.app.Application;
+import android.content.SharedPreferences;
+
+import net.ozero.drugreminders.DataStructures.Person;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class DrugRemindersApp extends Application {
+    List<Person> persons;
 
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        persons = new ArrayList<>();
+    }
 }
