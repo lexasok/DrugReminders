c9cbb3e6e385a737f9c10d3deb04101cc929e1ae
lexa <sokolov@ozero.net>
2018-04-25 02:22:06 +0300

    Field added

--- app/src/main/java/net/ozero/drugreminders/MainActivity.java
+++ app/src/main/java/net/ozero/drugreminders/MainActivity.java
@@ -6,12 +6,15 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.AlarmManagerCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+
+    RecyclerView mRV;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
