8ba237a0e1138747df0eee14bd8eecd275422121
lexa <sokolov@ozero.net>
2018-04-25 03:08:48 +0300

    Method added

--- app/src/main/java/net/ozero/drugreminders/adapters/MainActivityPersonsListAdapter.java
+++ app/src/main/java/net/ozero/drugreminders/adapters/MainActivityPersonsListAdapter.java
@@ -40,6 +40,11 @@ public class MainActivityPersonsListAdapter extends RecyclerView.Adapter<MainAct
         return persons.size();
     }
 
+    @Override
+    public void onAttachedToRecyclerView(@NonNull RecyclerView recyclerView) {
+        super.onAttachedToRecyclerView(recyclerView);
+    }
+
     public class PersonsViewHolder extends RecyclerView.ViewHolder{
         TextView name;
         TextView drugsCount;
