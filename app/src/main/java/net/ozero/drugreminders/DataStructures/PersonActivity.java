e5387e5dc742a8e3fb58fd0263b68b965dd1cb6b
lexa <sokolov@ozero.net>
2018-05-02 02:19:02 +0300

    Layout created

--- app/src/main/java/net/ozero/drugreminders/DataStructures/PersonActivity.java
+++ app/src/main/java/net/ozero/drugreminders/DataStructures/PersonActivity.java
@@ -1,6 +1,19 @@
 package net.ozero.drugreminders.DataStructures;
 
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
 public class PersonActivity extends AppCompatActivity {
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
 }
