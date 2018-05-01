efc5b7431f9c21bbb84ef13289da9c6cfafe1025
lexa <sokolov@ozero.net>
2018-05-02 02:23:50 +0300

    Layouts set

--- app/src/main/java/net/ozero/drugreminders/PersonActivity.java
+++ app/src/main/java/net/ozero/drugreminders/PersonActivity.java
@@ -1,14 +1,21 @@
-package net.ozero.drugreminders.DataStructures;
+package net.ozero.drugreminders;
 
 import android.os.Bundle;
 import android.os.PersistableBundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
 
 public class PersonActivity extends AppCompatActivity {
+
+    RecyclerView mRV;
+
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
         super.onCreate(savedInstanceState, persistentState);
+        setContentView(R.layout.activity_person);
+        mRV = findViewById(R.id.RVdrugListPersonActivity);
+        
     }
 
     @Override
