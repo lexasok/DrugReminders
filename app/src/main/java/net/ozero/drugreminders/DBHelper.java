990bd032722709c6926d6aaf8a405daea264381e
lexa <sokolov@ozero.net>
2018-05-23 19:37:09 +0300

    Constants added, method initialized

--- app/src/main/java/net/ozero/drugreminders/DBHelper.java
+++ app/src/main/java/net/ozero/drugreminders/DBHelper.java
@@ -6,11 +6,13 @@ import android.database.sqlite.SQLiteOpenHelper;
 
 public class DBHelper extends SQLiteOpenHelper {
 
+    public static final int DATABASE_VERSION = 1;
 
-    public static final String PERSONS_TABLE_NAME = "PERSONS";
-    public static final String NAME = "COLUMN_NAME";
+    public static final String DATABASE_NAME = "drugRemindersDb";
+    public static final String TABLE_PERSONS = "tablePersons";
+    public static final String COLUMN_NAME = "name";
 
-    private static final int DATABASE_VERSION = 1;
+
 
     public DBHelper(Context context) {
         super(context, "myDB", null, 1);
@@ -25,6 +27,7 @@ public class DBHelper extends SQLiteOpenHelper {
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-
+        db.execSQL("drop table if exists " + TABLE_PERSONS);
+        onCreate(db);
     }
 }
