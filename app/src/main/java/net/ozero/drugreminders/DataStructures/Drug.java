5d31ecbf3db04cede0760742dda54abec3bf8b2c
lexa <sokolov@ozero.net>
2018-05-24 13:37:03 +0300

    Last commit

--- app/src/main/java/net/ozero/drugreminders/DataStructures/Drug.java
+++ app/src/main/java/net/ozero/drugreminders/DataStructures/Drug.java
@@ -1,33 +1,13 @@
 package net.ozero.drugreminders.DataStructures;
 
 public class Drug {
+
     private String name;
-    private Dosage dosage;
-    private Schedule schedule;
+    private int dosage;
 
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Dosage getDosage() {
-        return dosage;
-    }
-
-    public void setDosage(Dosage dosage) {
-        this.dosage = dosage;
-    }
-
-    public Schedule getSchedule() {
-        return schedule;
-    }
-
-    public void setSchedule(Schedule schedule) {
-        this.schedule = schedule;
-    }
+//    private String name;
+//    private Dosage dosage;
+//    private Schedule schedule;
 
 
 }
--- app/src/main/java/net/ozero/drugreminders/DrugRemindersApp.java
+++ app/src/main/java/net/ozero/drugreminders/DrugRemindersApp.java
@@ -19,7 +19,6 @@ public class DrugRemindersApp extends Application {
     public void onCreate() {
         super.onCreate();
         dataManager = new DataManager(this);
-
     }
 
 
