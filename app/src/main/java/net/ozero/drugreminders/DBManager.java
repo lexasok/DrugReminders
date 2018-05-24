98a7ffe8992b60135cfa8657d0353c17a241919c
lexa <sokolov@ozero.net>
2018-05-24 08:43:39 +0300

    Constructor initialized, method declared

--- app/src/main/java/net/ozero/drugreminders/DBManager.java
+++ app/src/main/java/net/ozero/drugreminders/DBManager.java
@@ -1,4 +1,26 @@
 package net.ozero.drugreminders;
 
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+
+import net.ozero.drugreminders.DataStructures.Person;
+
+import java.util.ArrayList;
+
 public class DBManager {
+
+    private Context context;
+    private SQLiteDatabase database;
+    private DBHelper dbHelper;
+
+    public DBManager(Context context) {
+        this.context = context;
+        dbHelper = new DBHelper(context);
+        database = dbHelper.getWritableDatabase();
+    }
+
+    public ArrayList<Person> getPersonsList() {
+        return null;
+    }
+
 }
