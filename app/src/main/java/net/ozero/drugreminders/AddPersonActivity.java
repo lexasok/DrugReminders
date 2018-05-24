b886e8fe15e8028b2293b998c66fcdb6070f85dd
lexa <sokolov@ozero.net>
2018-05-24 09:40:10 +0300

    New Activity created

--- app/src/main/java/net/ozero/drugreminders/AddPersonActivity.java
+++ app/src/main/java/net/ozero/drugreminders/AddPersonActivity.java
@@ -1,13 +1,13 @@
 package net.ozero.drugreminders;
 
-import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 
 public class AddPersonActivity extends AppCompatActivity {
+
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        
+        setContentView(R.layout.activity_add_person);
     }
 }
