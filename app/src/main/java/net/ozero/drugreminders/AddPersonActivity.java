a1f228920033c3d6bf8c8f9860ea13f062cc7cea
lexa <sokolov@ozero.net>
2018-05-24 09:52:16 +0300

    onCreate initialized

--- app/src/main/java/net/ozero/drugreminders/AddPersonActivity.java
+++ app/src/main/java/net/ozero/drugreminders/AddPersonActivity.java
@@ -2,12 +2,28 @@ package net.ozero.drugreminders;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class AddPersonActivity extends AppCompatActivity {
+
+    private EditText editTextPersonNameInput;
+    private Button buttonAdd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_person);
+
+        buttonAdd = findViewById(R.id.buttonAdd);
+        editTextPersonNameInput = findViewById(R.id.editTextPersonName);
+
+        buttonAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
