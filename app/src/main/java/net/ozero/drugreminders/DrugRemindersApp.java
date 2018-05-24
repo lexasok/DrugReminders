274d3162f3117a6962f375adbcc54fc6ac892323
lexa <sokolov@ozero.net>
2018-05-24 08:50:47 +0300

    Old code deleted;

--- app/src/main/java/net/ozero/drugreminders/DrugRemindersApp.java
+++ app/src/main/java/net/ozero/drugreminders/DrugRemindersApp.java
@@ -20,43 +20,11 @@ public class DrugRemindersApp extends Application {
     public void onCreate() {
         super.onCreate();
 
-        //database
-        dbHelper = new DBHelper(this);
-        ContentValues contentValues = new ContentValues();
-        SQLiteDatabase database = dbHelper.getWritableDatabase();
-
-        Cursor cursor = database.query(
-                DBHelper.TABLE_PERSONS,
-                null, null,
-                null, null,
-                null, null
-        );
-
-        personsNames = new ArrayList<>();
-
-        boolean moveToFirst = cursor.moveToFirst();
-
-        if (moveToFirst) {
-            int nameColumnIndex = cursor.getColumnIndex(DBHelper.KEY_NAME);
-
-            do {
-                personsNames.add(cursor.getString(nameColumnIndex));
-            } while (cursor.moveToNext());
-        }
-
-
-        persons = new ArrayList<>();
-        //DATA from DB
-        loadPersonsFromDB(persons, personsNames);
 
     }
 
 
-    private void loadPersonsFromDB(List<Person> persons, ArrayList<String> personsNames) {
-        for (int i = 0; i < personsNames.size(); i++) {
-            persons.add(new Person(personsNames.get(i)));
-        }
-    }
+
 
     public List<Person> getPersons() {
         return persons;
